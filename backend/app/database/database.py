08fa5465e80c1837d06a0c6ae14f224845424ce1
Alceu Pereira <alceupereira3105@gmail.com>
2026-06-10 22:30:08 -0300

    fetch: criando as tabelas barbeiros e agendamentos do banco de dados

--- backend/app/database/database.py
+++ backend/app/database/database.py
@@ -3,11 +3,15 @@ import sqlite3
 DATABASE_NAME = "barbearia.db"
 
 def get_connection():
-    return sqlite3.connect(DATABASE_NAME)
+    connection = sqlite3.connect(DATABASE_NAME)
+    connection.execute("PRAGMA foreign_keys = ON")
+    return connection
+
 
 def create_tables():
     connection = get_connection()
     cursor = connection.cursor()
+
     cursor.execute(
         """
         CREATE TABLE IF NOT EXISTS clientes (
@@ -15,6 +19,30 @@ def create_tables():
             nome TEXT NOT NULL,
             telefone TEXT NOT NULL
         )
+        """
+    )
+    cursor.execute(
+        """
+        CREATE TABLE IF NOT EXISTS barbeiros (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            nome TEXT NOT NULL
+            )
+
+        """
+    )
+    cursor.execute(
+        """
+        CREATE TABLE IF NOT EXISTS agendamentos (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            data DATE NOT NULL,
+            hora TIME NOT NULL,
+            status VARCHAR(100) NOT NULL,
+            cliente_id INTEGER NOT NULL,
+            barbeiro_id INTEGER NOT NULL,
+
+            FOREIGN KEY (cliente_id) REFERENCES clientes(id),
+            FOREIGN KEY (barbeiro_id) REFERENCES barbeiros(id)
+            )
         """
     )
 
